d9277203a6267812670b8520b8f821ce71dbc794
Prashant Singh <ps731278@gmail.com>
2025-03-26 16:36:05 +0530

    Build MainPage and Started rebuilding MyPortfolio

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "../App.css";
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <nav>
+        <div className="left-nav">
+          <span>&lt;Prashant Singh /&gt;</span>
+        </div>
+        <div className="middle-nav">
+          <div className="switch">
+            <img src="/sun.svg" alt="_sun" />
+          </div>
+        </div>
+        <div className="right-nav">
+          <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>About Me</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav>
+      <main>
+        <div className="left-main">
+          <h1>
+            I'm
+            <br />a<br />
+            Web
+            <br />
+            <span style={{ fontWeight: 500,color:"skyblue" }}>Developer</span>&lt;/&gt;
+          </h1>
+          <p>Good Morning :)</p>
+          <p>
+            I'm Prashant,a passionate web developer with a strong foundation in
+            Modern Web Technologies, Including
+            JavaScript,React.js,Express.js,Node.js.
+          </p>
+          <div className="contact-box">
+            <div className="social-icons">
+              <img src="/linkedin.svg" alt="_linkedin" />
+            </div>
+            <div className="social-icons">
+              <img src="/github.svg" alt="_github" />
+            </div>
+            <div className="social-icons">
+              <img src="/discord.svg" alt="_discord" />
+            </div>
+          </div>
+           <div className="contact-btns">
+            <button>Hire Me</button>
+            <button>Resume</button>
+           </div>
+        </div>
+        <div className="right-main">
+           <div id="profile"> 
+            <img src="/MyProfileImage.webp" alt="" srcset="" />
+            </div>
+         </div>
+      </main>
+       <hr />
+    </div>
+  );
+};
+
+export default MainPage;
